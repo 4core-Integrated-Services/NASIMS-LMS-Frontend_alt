dcb03d48b8f0933dd07a8b552aea1ea5efa8f50e
Wisdom Ekeh <89356694+wisdomsGit21@users.noreply.github.com>
2022-11-23 14:08:23 +0100

    Update [instructorId].tsx

--- pages/instructor-details/[instructorId].tsx
+++ pages/instructor-details/[instructorId].tsx
@@ -1,31 +1,31 @@
-import React, { ReactElement, useCallback } from 'react';
-import PageHeader from '../../components/NavMenu/PageHeader';
-import InstructorDetails from '../../components/Instuctor/InstructorDetails';
-import { useRouter } from 'next/router';
-import { NextPageWithLayout } from '../_app';
-import { PageWrapper } from '../../src/layout/page.wrapper';
-import { HomeLayout } from '../../src/layout';
-import instructorObject from '../../components/data/instructors';
+import React, { ReactElement, useCallback } from "react";
+import PageHeader from "../../components/NavMenu/PageHeader";
+import InstructorDetails from "../../components/Instuctor/InstructorDetails";
+import { useRouter } from "next/router";
+import { NextPageWithLayout } from "../_app";
+import { PageWrapper } from "../../src/layout/page.wrapper";
+import { HomeLayout } from "../../src/layout";
+import instructorObject from "../../components/data/instructors";
 
 const InstructorPage: NextPageWithLayout = () => {
   const {
-    query: { instructorId }
+    query: { instructorId },
   } = useRouter();
   const instructor = useCallback(() => {
     try {
       return instructorObject.find(
-        (data) => data.id === parseInt(instructorId.toString(), 10)
+        (data) => parseInt(data.id) === parseInt(instructorId.toString(), 10)
       );
     } catch (e) {
       return {
-        instructorName: ''
+        instructorName: "",
       };
     }
   }, [instructorId]);
 
   return (
     <PageWrapper
-      title={`${instructor()?.instructorName ?? ''} Instructor Nasims Learning`}
+      title={`${instructor()?.instructorName ?? ""} Instructor Nasims Learning`}
     >
       <PageHeader title="Instructor Details" />
       <InstructorDetails instructorId={instructorId} />
